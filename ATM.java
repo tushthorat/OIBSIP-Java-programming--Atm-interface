79a9ea154e84cb17d4f3ad0e4efcb9076930d267
Tushar Bhausaheb Thorat <126282133+tushthorat@users.noreply.github.com>
2024-02-21 23:13:40 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -8,29 +8,36 @@ import java.util.Scanner;
 public class ATM{
     
     public static void main(String args[]){
+        
         System.out.println("WELCOME TO ATM...");
         System.out.println("\nSelect any one option!");
         System.out.println("1.REGISTER\n12EXIT");
         System.out.println("Enter your option :");
         Scanner sc=new Scanner(System.in);
+        
         int ch;
         ch=sc.nextInt();
+        
         if (ch==1){
             ATMFUNCTIONS Atm=new ATMFUNCTIONS();
             Atm.register();
+            
             while(ch==1){
                 System.out.println("\nSelect any one option!");
                 System.out.println("1.LOGIN\n2.EXIT");
                 System.out.println("Enter your option :");
                 int op=sc.nextInt();
+                
                 if (op==1){
                     if(Atm.login()){
                         boolean isFin=false;
+                        
                         while(!isFin){
                             System.out.println("\nSelect any option : ");
                             System.out.println("1.DEPOSIT\n2.WITHDRAW\n3.TRANSFER\n4.TRANSACTION HISTORY\n5.CHECK BALANCE\n6.EXIT");
                             System.out.println("Enter your option :");
                             int c=sc.nextInt();
+                            
                             switch(c){
                                 case 1 : Atm.deposit();
                                          break;
